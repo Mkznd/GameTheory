c49acbd67bc33cb516f39557164295576459079e
Mkznd <yzundai1@gmail.com>
2024-01-11 21:24:29 +0100

    Updated main

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ import asyncio
 
 def main():
     conns = [
-        Connection(Node("a"), Node("b"), lambda x: x),
+        Connection(Node("a"), Node("b"), lambda x: x+1),
         Connection(Node("a"), Node("c"), lambda x: x),
         Connection(Node("b"), Node("e"), lambda x: 2),
-        Connection(Node("c"), Node("e"), lambda x: x),
+        Connection(Node("c"), Node("e"), lambda x: x+2),
     ]
     (paths, lens) = asyncio.run(Graph(conns).find_path_for_population("a", "e", 5))
-    
-    
+    print(paths, sum(lens)/len(lens))
 
 
 if __name__ == "__main__":
